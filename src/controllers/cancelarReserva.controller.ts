2612a338025782edf3a5389a3d9dac659c692891
Kaua Nyllo da Silva Guimaraes <01840915@sempreuninassau.com.br>
2026-05-13 19:17:40 -0300

    update: diminui a mensagem do cancelarReservaController

--- src/controllers/cancelarReserva.controller.ts
+++ src/controllers/cancelarReserva.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export const cancelarReservaController = async (req: Request, res: Response) => {
-    return res.status(200).json({ message: "Reserva cancelada" });
+    return res.status(200).json({ message: "Reserva" });
 }
