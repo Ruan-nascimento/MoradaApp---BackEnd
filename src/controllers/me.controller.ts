7f6a2a1fa76bd85ba820557363a35961247f0b35
PlayRunOffc <playruns3@gmail.com>
2026-05-23 20:20:40 -0300

    fix: update user fetching logic in meController

--- src/controllers/me.controller.ts
+++ src/controllers/me.controller.ts
@@ -6,29 +6,39 @@ export const meController = async (req: AuthRequest, res: Response) => {
 
     try {
 
-        const user = await prisma.usuario.findUnique({
-            where: {
-                id: req.userId,
-            },
-            select: {
-                id: true,
-                email: true,
-                name: true,
-                reservas: true
-            }
-        });
+        try {
 
-        if (!user) {
-            return res.status(400).json({
-                message: "Usuário não encontrado",
+            const user = await prisma.usuario.findFirst({
+                where: {
+                    id: req.userId,
+                },
+                select: {
+                    id: true,
+                    email: true,
+                    name: true,
+                }
+            });
+            if (!user) {
+                return res.status(400).json({
+                    message: "Usuário não encontrado",
+                    success: false
+                });
+            }
+    
+            return res.status(200).json({
+                user,
+                success: true
+            });
+        } catch (error) {
+            console.error("Error fetching user:", error);
+            return res.status(500).json({
+                message: "Erro ao buscar usuário. Tente Novamente!",
                 success: false
             });
         }
 
-        return res.status(200).json({
-            user,
-            success: true
-        });
+
+
 
     } catch (error) {
         return res.status(500).json({
